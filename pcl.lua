bd4f2ddbe456001f1e670d5259b6688b229c253b
Andreas Köpf <andreas.koepf@xamla.com>
2015-10-22 01:40:05 +0200

    Add basic point type definitions

--- pcl.lua
+++ pcl.lua
@@ -1,16 +1,36 @@
+require 'torch'
 local ffi = require 'ffi'
 
-ffi.cdef[[
+local ALIGN16 = "__attribute__((aligned(16)))"
+local PCL_POINT4D = "union ".. ALIGN16.." { float data[4]; struct { float x; float y; float z; }; };"
+local PCL_NORMAL4D = "union "..ALIGN16.." { float data_n[4]; float normal[3]; struct { float normal_x; float normal_y; float normal_z; }; };"
+local PCL_RGB = "union { union { struct { uint8_t b; uint8_t g; uint8_t r; uint8_t a; }; uint32_t rgba; }; uint32_t rgba; };"
 
-typedef unsigned int		uint32_t;
-
+local cdef = "typedef struct RGB { "..PCL_RGB.." } RGB; \z
+typedef struct PointXY { float x; float y; } PointUV; \z
+typedef struct PointUV { float u; float v; } PointUV; \z
+typedef struct "..ALIGN16.." InterestPoint { "..PCL_POINT4D.." union { struct { float strength; }; float data_c[4]; }; } InterestPoint; \z
+typedef struct "..ALIGN16.." _PointXYZ { "..PCL_POINT4D.."} _PointXYZ; \z
+typedef struct "..ALIGN16.." _PointXYZI { "..PCL_POINT4D.." union { struct { float intensity; }; float data_c[4]; }; } _PointXYZI; \z
+typedef struct "..ALIGN16.." _PointXYZL { "..PCL_POINT4D.." uint32_t label; } _PointXYZL; \z
+typedef struct "..ALIGN16.." _PointXYZRGBA { "..PCL_POINT4D..PCL_RGB.." } _PointXYZRGBA; \z
+typedef struct "..ALIGN16.." _PointXYZRGBL { "..PCL_POINT4D..PCL_RGB.." uint32_t label; } _PointXYZRGBL; \z
+typedef struct "..ALIGN16.." _Normal { "..PCL_POINT4D.." union { struct { float curvature; }; float data_c[4]; }; } _Normal; \z
+typedef struct "..ALIGN16.." _Axis { "..PCL_NORMAL4D.." } _Axis; \z
+typedef struct "..ALIGN16.." _PointNormal { "..PCL_POINT4D..PCL_NORMAL4D.." union { struct { float curvature; }; float data_c[4]; }; } _PointNormal; \z
+typedef struct "..ALIGN16.." _PointXYZRGBNormal { "..PCL_POINT4D..PCL_NORMAL4D.." union { struct { "..PCL_RGB.." float curvature; }; float data_c[4]; }; } _PointXYZRGBNormal; \z
+typedef struct "..ALIGN16.." _PointXYZINormal { "..PCL_POINT4D..PCL_NORMAL4D.." union { struct { float intensity; float curvature; }; float data_c[4]; }; } _PointXYZINormal;" ..
+[[
 void* pcl_PointCloud_XYZ_new(uint32_t width, uint32_t height);
 void pcl_PointCloud_XYZ_delete(void* self);
 bool pcl_PointCloud_XYZ_empty(void* self);
 void pcl_PointCloud_XYZ_clear(void* self);
+_PointXYZ& pcl_PointCloud_at1D(void* self, int n);
 void pcl_PointCloud_XYZ_totensor(void* self, void* destination);
 THFloatStorage* pcl_PointCloud_XYZ_storage(void* self);
 ]]
+
+ffi.cdef(cdef)
 
 local p = ffi.load('build/debug/libtorch-pcl.so')
 
@@ -29,29 +49,34 @@ local types = {
     "XYZINormal"      -- float x, y, z, intensity, normal[3], curvature;
 }
 
-
 function PointCloud:__tostring__()
     return "PointCloud"
 end
 
 
 local obj = p.pcl_PointCloud_XYZ_new(10, 10)
+print(torch.typename(obj))
 ffi.gc(obj, p.pcl_PointCloud_XYZ_delete)
 
-local  a = p.pcl_PointCloud_XYZ_empty(obj)
-print(a)
---p.pcl_PointCloud_XYZ_clear(obj)
-local  b = p.pcl_PointCloud_XYZ_empty(obj)
-print(b)
+local x =p.pcl_PointCloud_at1D(obj, 3)
+--x.y = 1.23124;
+
+
+local y =p.pcl_PointCloud_at1D(obj, 3)
+print(y.y)
 
 
 local s = p.pcl_PointCloud_XYZ_storage(obj)
-print(s)
-local t =torch.FloatTensor()
-t:cdata().storage = t
-print(t)
+local t = torch.FloatTensor()
+t:cdata().storage = s
+
+
+
 local s2 = t:storage()
-print(s2)
+--print(s2)
+--print(s2:cdata().size)
+--print(s2:cdata())
+--print(s)
 
 
 --local ft = torch.FloatTensor()
--- src/io.cpp
+++ src/io.cpp
@@ -16,12 +16,12 @@ extern "C" void pcl_PointCloud_XYZ_delete(pcl::PointCloud<pcl::PointXYZ>* self)
     delete self;
 }
 
-/*extern "C" pcl_PointCloud_at1D(pcl::PointCloud<pcl::PointXYZ>* self, int n)
+extern "C" pcl::PointXYZ& pcl_PointCloud_at1D(pcl::PointCloud<pcl::PointXYZ>* self, int n)
 {
     return self->at(n);
 }
 
-extern "C" pcl_PointCloud_at2D(pcl::PointCloud<pcl::PointXYZ>* self, int column, int row)
+/*extern "C" pcl_PointCloud_at2D(pcl::PointCloud<pcl::PointXYZ>* self, int column, int row)
 {
     return self->at(column, row);
 }*/
@@ -48,26 +48,11 @@ extern "C" bool pcl_PointCloud_XYZ_isOrganized(pcl::PointCloud<pcl::PointXYZ>* s
 extern "C" THFloatStorage* pcl_PointCloud_XYZ_storage(pcl::PointCloud<pcl::PointXYZ>* self)
 {
     float* ptr = reinterpret_cast<float*>(&self->points[0]);
-    THFloatStorage* storage = THFloatStorage_newWithData(ptr, self->points.size() * sizeof(pcl::PointXYZ));
-    storage->refcount = 1;
+    THFloatStorage* storage = THFloatStorage_newWithData(ptr, self->points.size() * sizeof(pcl::PointXYZ) / sizeof(float));
     storage->flag = TH_STORAGE_REFCOUNTED;
     return storage;
 }
 
-/*extern "C" void pcl_PointCloud_XYZ_tensormap(pcl::PointCloud<pcl::PointXYZ>* self, void* destination, int dim, int stride, int offset)
-{
-    THTensor* d = static_cast<THTensor*>(destination);
-    
-    //reinterpret_cast<float*>(&self->points[0]) + offset, 
-    //self->points.size(), dim, 
-    //stride
-}
-
-extern "C" void pcl_PointCloud_XYZ_totensor(pcl::PointCloud<pcl::PointXYZ>* self, void* destination)
-{
-    pcl_PointCloud_XYZ_tensormap(self, destination, sizeof(pcl::PointXYZ) / sizeof(float), sizeof(pcl::PointXYZ) / sizeof(float), 0));
-}
-*/
 //extern "C" pcl_PointCloud_XYZ_op_add(pcl::PointCloud<pcl::PointXYZ>* self, pcl::PointCloud<pcl::PointXYZ>* other)
 //{
 //}
