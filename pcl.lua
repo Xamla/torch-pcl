e85d8be33c5d9c95e87be2d492d281d91f8e09ab
Andreas Köpf <andreas.koepf@xamla.com>
2015-10-21 18:38:09 +0200

    Add first experiments with ffi

--- pcl.lua
+++ pcl.lua
@@ -1,21 +1,77 @@
 local ffi = require 'ffi'
 
-function libPath(libName)
-    if     ffi.os == 'Windows' then
-        return 'lib/' .. libName .. '.dll'
-    elseif ffi.os == 'OSX' then
-        return 'lib/lib' .. libName .. '.dylib'
-    else
-        return 'lib/lib' .. libName .. '.so'
-    end
-end
-
 ffi.cdef[[
 
+typedef unsigned int		uint32_t;
+
+void* pcl_PointCloud_XYZ_new(uint32_t width, uint32_t height);
+void pcl_PointCloud_XYZ_delete(void* self);
+bool pcl_PointCloud_XYZ_empty(void* self);
+void pcl_PointCloud_XYZ_clear(void* self);
+void pcl_PointCloud_XYZ_totensor(void* self, void* destination);
+THFloatStorage* pcl_PointCloud_XYZ_storage(void* self);
 ]]
 
-local C = ffi.load(libPath('Common'))
+local p = ffi.load('build/debug/libtorch-pcl.so')
 
-pcl = {}
+
+local PointCloud = {}
+
+local types = {
+    "XYZ",            -- float x, y, z;    
+    "XYZI",           -- float x, y, z, intensity;
+    "XYZRGBA",        -- float x, y, z; uint32_t rgba;
+    "XYZRGB",         -- float x, y, z, rgb;
+    "XY",             -- float x, y;
+    "Normal",         -- float normal[3], curvature;
+    "XYZNormal",      -- float x, y, z; float normal[3], curvature;
+    "XYZRGBNormal",   -- float x, y, z, rgb, normal[3], curvature;
+    "XYZINormal"      -- float x, y, z, intensity, normal[3], curvature;
+}
+
+
+function PointCloud:__tostring__()
+    return "PointCloud"
+end
+
+
+local obj = p.pcl_PointCloud_XYZ_new(10, 10)
+ffi.gc(obj, p.pcl_PointCloud_XYZ_delete)
+
+local  a = p.pcl_PointCloud_XYZ_empty(obj)
+print(a)
+--p.pcl_PointCloud_XYZ_clear(obj)
+local  b = p.pcl_PointCloud_XYZ_empty(obj)
+print(b)
+
+
+local s = p.pcl_PointCloud_XYZ_storage(obj)
+print(s)
+local t =torch.FloatTensor()
+t:cdata().storage = t
+print(t)
+local s2 = t:storage()
+print(s2)
+
+
+--local ft = torch.FloatTensor()
+--p.pcl_PointCloud_XYZ_totensor(ft:cdata())
+
+--print(ft)
+
+
+
+-- converting a torch tensor into a point cloud type
+local pt = torch.rand(3, 10)
+--p.PointCloud
+
+
+-- loading a point cloud file and get points as torch tensors
+--local cloud = pcl.io.loadPCDFile('')
+
+--pcl.PointCloud('XYZ')
+
+-- allocating / deallocating objects
+
 
 return pcl
--- src/io.cpp
+++ src/io.cpp
@@ -2,8 +2,72 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
-
-int main (int argc, char** argv)
-{
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+extern "C" {
+#include <TH/TH.h>
 }
+
+extern "C" void* pcl_PointCloud_XYZ_new(uint32_t width, uint32_t height)
+{
+    return new pcl::PointCloud<pcl::PointXYZ>(width, height);
+}
+
+extern "C" void pcl_PointCloud_XYZ_delete(pcl::PointCloud<pcl::PointXYZ>* self)
+{
+    delete self;
+}
+
+/*extern "C" pcl_PointCloud_at1D(pcl::PointCloud<pcl::PointXYZ>* self, int n)
+{
+    return self->at(n);
+}
+
+extern "C" pcl_PointCloud_at2D(pcl::PointCloud<pcl::PointXYZ>* self, int column, int row)
+{
+    return self->at(column, row);
+}*/
+
+// set_at1D
+// set_at2D
+
+
+extern "C" void pcl_PointCloud_XYZ_clear(pcl::PointCloud<pcl::PointXYZ>* self)
+{
+    self->clear();
+}
+
+extern "C" bool pcl_PointCloud_XYZ_empty(pcl::PointCloud<pcl::PointXYZ>* self)
+{
+    return self->empty();
+}
+
+extern "C" bool pcl_PointCloud_XYZ_isOrganized(pcl::PointCloud<pcl::PointXYZ>* self)
+{
+    return self->isOrganized();
+}
+
+extern "C" THFloatStorage* pcl_PointCloud_XYZ_storage(pcl::PointCloud<pcl::PointXYZ>* self)
+{
+    float* ptr = reinterpret_cast<float*>(&self->points[0]);
+    THFloatStorage* storage = THFloatStorage_newWithData(ptr, self->points.size() * sizeof(pcl::PointXYZ));
+    storage->refcount = 1;
+    storage->flag = TH_STORAGE_REFCOUNTED;
+    return storage;
+}
+
+/*extern "C" void pcl_PointCloud_XYZ_tensormap(pcl::PointCloud<pcl::PointXYZ>* self, void* destination, int dim, int stride, int offset)
+{
+    THTensor* d = static_cast<THTensor*>(destination);
+    
+    //reinterpret_cast<float*>(&self->points[0]) + offset, 
+    //self->points.size(), dim, 
+    //stride
+}
+
+extern "C" void pcl_PointCloud_XYZ_totensor(pcl::PointCloud<pcl::PointXYZ>* self, void* destination)
+{
+    pcl_PointCloud_XYZ_tensormap(self, destination, sizeof(pcl::PointXYZ) / sizeof(float), sizeof(pcl::PointXYZ) / sizeof(float), 0));
+}
+*/
+//extern "C" pcl_PointCloud_XYZ_op_add(pcl::PointCloud<pcl::PointXYZ>* self, pcl::PointCloud<pcl::PointXYZ>* other)
+//{
+//}
